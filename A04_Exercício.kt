4a117798c5c86320c5021fec1277667c79c51489
SamuelBomfim <samucasbomfim@gmail.com>
2024-09-25 22:54:58 -0300

    Add files via upload

---
+++ A04_Exercício.kt
@@ -0,0 +1,20 @@
+fun main() {
+    // B:
+    var name: String = "Samuel Bomfim"
+
+    // C:
+    var text: String? = null
+
+    // D:
+    val numeroValor: Byte = 40
+
+    // E:
+    val PIBbr: Long = 10_900_000_000_000_000L
+
+    // F:
+    val habtBr: Int = 212_600_000
+
+    // G:
+    val pibCapita = PIBbr / habtBr
+    println("PIB per capita: R$$pibCapita")
+}
